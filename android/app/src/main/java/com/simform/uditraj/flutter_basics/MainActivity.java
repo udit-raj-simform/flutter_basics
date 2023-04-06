5e6f18d7861903323016fe0b3cd795a1f3fa3a81
Udit Raj <udit.r@simformsolutions.com>
2023-04-06 12:17:33 +0530

    Initial Commit added DefaultTabController added TabBar and TabBarView added PopupMenuButton

---
+++ android/app/src/main/java/com/simform/uditraj/flutter_basics/MainActivity.java
@@ -0,0 +1,6 @@
+package com.simform.uditraj.flutter_basics;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_basics/pages/sliver_app_bar.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Basics",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true),
+      home: const Home(),
+    );
+  }
+}
+
+enum MyItems { sliverAppbar, itemTwo, itemThree }
+
+class Home extends StatefulWidget with PreferredSizeWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+
+  @override
+  Size get preferredSize => throw UnimplementedError();
+}
+
+class _HomeState extends State<Home> {
+  MyItems? selectedItem;
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+            leading: const Icon(Icons.menu),
+            title: const Text(
+              "Basic Widgets and Layout",
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            centerTitle: true,
+            actions: [
+              const Icon(Icons.search),
+              PopupMenuButton<MyItems>(
+                initialValue: selectedItem,
+                onSelected: (MyItems item) {
+                  setState(() {
+                    selectedItem = item;
+                  });
+                },
+                itemBuilder: (BuildContext context) =>
+                    <PopupMenuEntry<MyItems>>[
+                  PopupMenuItem<MyItems>(
+                      value: MyItems.sliverAppbar,
+                      onTap: (selectedItem == MyItems.sliverAppbar)
+                          ? Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const SliverAppBarPage(),
+                              ),
+                            )
+                          : doNothing(),
+                      child: const Text('Sliver')),
+                  const PopupMenuItem<MyItems>(
+                    value: MyItems.itemTwo,
+                    child: Text('Item 2'),
+                  ),
+                  const PopupMenuItem<MyItems>(
+                    value: MyItems.itemThree,
+                    child: Text('Item 3'),
+                  ),
+                ],
+              )
+            ],
+            bottom: const TabBar(tabs: [
+              Tab(
+                text: "Messages",
+              ),
+              Tab(
+                text: "Stories",
+              ),
+              Tab(
+                text: "Calls",
+              ),
+            ])),
+        body: const TabBarView(
+          children: [
+            Icon(Icons.home_filled),
+            Icon(Icons.location_history_rounded),
+            Icon(Icons.call_end_rounded)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+doNothing() {
+  return;
+}
